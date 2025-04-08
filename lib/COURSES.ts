8974567eea55081d7f387af7c29a28b7c31a656f
Alekanedero <alexandrecompin@gmail.com>
2025-04-08 16:31:44 +0200

    #10 - updates courses

--- lib/COURSES.ts
+++ lib/COURSES.ts
@@ -32,7 +32,7 @@ export const COURSES = [
     },
     urlSchool: "https://codelynx.dev/beginreact",
     title: "Begin react",
-    description: "Melvin's online platform",
+    description: "Melvyn Malherbe - Codelynx",
     text: "I continued my apprenticeship with training on Melvin's Codeline platform. This course enabled me to review, practise and deepen my understanding of the main concepts of React and the various design patterns. Thanks to an interactive approach combining exercises and workshops, I was able to consolidate my skills and improve my understanding of this library.",
   },
   {
@@ -51,6 +51,6 @@ export const COURSES = [
     urlSchool: "https://codelynx.dev/nextreact",
     title: "Next React",
     text: "I then took a full course in Next.js. During this apprenticeship, I strengthened my mastery of TypeScript and React, structuring my projects according to professional best practice and using advanced tools such as Prisma, NextAuth and TanStack Query. I also worked on managing Server and Client Components, advanced routing, securing Server Actions, and integrating tests in React to ensure code reliability. Finally, I learned how to deploy my projects on Vercel and optimise them. A concrete project enabled me to put these skills into practice.",
-    description: "Melvin's online platform",
-  }
+    description: "Melvyn Malherbe - Codelynx",
+  },
 ];
